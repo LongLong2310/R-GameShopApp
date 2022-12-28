528d0a8deeb4fa28aa694bc9dd8ed99779b538a3
quankun <s3877457@rmit.edu.vn>
2022-12-28 10:53:33 +0700

    account screen and function complete

--- app/src/main/java/com/example/r_gameshopapp/DatabaseManager.java
+++ app/src/main/java/com/example/r_gameshopapp/DatabaseManager.java
@@ -34,12 +34,11 @@ public class DatabaseManager {
         database.insert(DatabaseHelper.TABLE_NAME_S, null, contentValue);
     }
 
-    public void insertAccount(int id, String username, String pass, int cash){
+    public void insertAccount( String username, String pass, double cash){
         ContentValues contentValue = new ContentValues();
-        contentValue.put(DatabaseHelper.ID, id);
         contentValue.put(DatabaseHelper.NAME, username);
         contentValue.put(DatabaseHelper.PASS, pass);
-        contentValue.put(DatabaseHelper.CASH, cash);
+        contentValue.put(DatabaseHelper.CASH, "$" +cash);
         database.insert(DatabaseHelper.TABLE_NAME_A, null, contentValue);
     }
 
@@ -63,11 +62,11 @@ public class DatabaseManager {
         database.insert(DatabaseHelper.TABLE_NAME_H, null, contentValue);
     }
 
-    public int updateAccount(long _id,String username, String pass, int cash) {
+    public int updateAccount(long _id,String username, String pass, double cash) {
         ContentValues contentValue = new ContentValues();
         contentValue.put(DatabaseHelper.NAME, username);
         contentValue.put(DatabaseHelper.PASS, pass);
-        contentValue.put(DatabaseHelper.CASH, cash);
+        contentValue.put(DatabaseHelper.CASH, "$" + cash);
         int i = database.update(DatabaseHelper.TABLE_NAME_A,
                 contentValue,
                 DatabaseHelper.ID + " =" + _id, null);
@@ -225,4 +224,86 @@ public class DatabaseManager {
         }
         return cursor;
     }
+    public Cursor selectAllAccount(){
+        String [] columns = new String[] {
+                DatabaseHelper.ID,
+                DatabaseHelper.NAME,
+                DatabaseHelper.PASS,
+                DatabaseHelper.CASH
+        };
+        Cursor cursor = database.query(DatabaseHelper.TABLE_NAME_A, columns,
+                null, null, null, null, null);
+
+        if (cursor != null){
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+    public boolean checkUser(String username) {
+
+        // array of columns to fetch
+        String[] columns = {
+                DatabaseHelper.ID
+        };
+        ;
+        // selection criteria
+        String selection = DatabaseHelper.NAME + " = ?";
+        // selection argument
+        String[] selectionArgs = {username};
+        // query user table with condition
+        /**
+         * Here query function is used to fetch records from user table this function works like we use sql query.
+         * SQL query equivalent to this query function is
+         * SELECT user_id FROM user WHERE user_name = 'jack@androidtutorialshub.com';
+         */
+        Cursor cursor = database.query(DatabaseHelper.TABLE_NAME_A, //Table to query
+                columns,                    //columns to return
+                selection,                  //columns for the WHERE clause
+                selectionArgs,              //The values for the WHERE clause
+                null,                       //group the rows
+                null,                      //filter by row groups
+                null);                      //The sort order
+        int cursorCount = cursor.getCount();
+        cursor.close();
+        if (cursorCount > 0) {
+            return true;
+        }
+        return false;
+    }
+    public boolean checkUser(String username, String password) {
+        // array of columns to fetch
+        String[] columns = {
+                DatabaseHelper.ID
+        };
+        if (dbHelper == null) {
+            dbHelper = new DatabaseHelper(context);
+            database = dbHelper.getWritableDatabase();
+        }
+        // selection criteria
+        String selection = DatabaseHelper.NAME + " = ?" + " AND " + DatabaseHelper.PASS + " = ?";
+        // selection arguments
+        String[] selectionArgs = {username, password};
+        // query user table with conditions
+        /**
+         * Here query function is used to fetch records from user table this function works like we use sql query.
+         * SQL query equivalent to this query function is
+         * SELECT user_id FROM user WHERE user_name= AND user_password = 'qwerty';
+         */
+        Cursor cursor = database.query(DatabaseHelper.TABLE_NAME_A, //Table to query
+                columns,                    //columns to return
+                selection,                  //columns for the WHERE clause
+                selectionArgs,              //The values for the WHERE clause
+                null,                       //group the rows
+                null,                       //filter by row groups
+                null);                      //The sort order
+        int cursorCount = cursor.getCount();
+        cursor.close();
+        if (dbHelper != null) database.close();
+        if (cursorCount > 0) {
+            return true;
+        }
+        return false;
+    }
+
 }
