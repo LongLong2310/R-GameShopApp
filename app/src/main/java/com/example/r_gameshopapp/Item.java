b840be4d562ef8d137e6a1c7149807193c29b4bc
XcSteven <73635652+XcSteven@users.noreply.github.com>
2022-12-29 10:40:49 +0700

    Update GridAdapter.java and Item.java

--- app/src/main/java/com/example/r_gameshopapp/Item.java
+++ app/src/main/java/com/example/r_gameshopapp/Item.java
@@ -3,15 +3,29 @@ package com.example.r_gameshopapp;
 public class Item {
 
     String itemListName;
+    int itemListStock;
+    String itemListCategory;
+    double itemListPrice;
     int itemListImage;
 
-    public Item(String itemName,int itemImage)
-    {
-        this.itemListImage = itemImage;
+    public Item(String itemName, int itemStock, String itemCategory, double itemPrice, int itemImage) {
         this.itemListName = itemName;
+        this.itemListStock = itemStock;
+        this.itemListCategory = itemCategory;
+        this.itemListPrice = itemPrice;
+        this.itemListImage = itemImage;
     }
     public String getitemName() {
         return itemListName;
+    }
+    public int getiemStock() {
+        return itemListStock;
+    }
+    public String getitemCategory() {
+        return itemListCategory;
+    }
+    public double getitemPrice() {
+        return itemListPrice;
     }
     public int getitemImage() {
         return itemListImage;
