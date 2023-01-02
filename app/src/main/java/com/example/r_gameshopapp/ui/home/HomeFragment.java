56c21d24ef8c21b7f9b8edfd7ca599a735dcd1ca
MrToilet <chitoan.tran@yahoo.com.vn>
2023-01-02 21:23:23 +0700

    Add popup Item Detail

--- app/src/main/java/com/example/r_gameshopapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/r_gameshopapp/ui/home/HomeFragment.java
@@ -6,20 +6,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.r_gameshopapp.GridAdapter;
 import com.example.r_gameshopapp.Item;
+import com.example.r_gameshopapp.MainActivity;
 import com.example.r_gameshopapp.R;
 import com.example.r_gameshopapp.databinding.FragmentHomeBinding;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
+
+    private AlertDialog.Builder dialogbuilder;
+    private AlertDialog dialog;
+    private TextView itemName, itemStock, itemCategory, itemPrice, amount;
+    private ImageButton button_cancel;
+    private Button  button_add_to_cart;
 
     private FragmentHomeBinding binding;
     GridView gridList;
@@ -42,11 +56,35 @@ public class HomeFragment extends Fragment {
         gridList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+                Toast.makeText(context, "wrong password or username", Toast.LENGTH_SHORT).show();
+                createItemDetailDialog(context);
             }
         });
 
         return root;
+    }
+
+    public void createItemDetailDialog(Context context){
+        dialogbuilder = new AlertDialog.Builder(context);
+        final View itemDetailPopupView = getLayoutInflater().inflate(R.layout.item_detail, null);
+        itemName = (TextView) itemDetailPopupView.findViewById(R.id.item_name);
+        itemStock = (TextView) itemDetailPopupView.findViewById(R.id.item_stock);
+        itemPrice = (TextView) itemDetailPopupView.findViewById(R.id.item_price);
+        amount = (EditText) itemDetailPopupView.findViewById(R.id.amount);
+
+        button_cancel = (ImageButton) itemDetailPopupView.findViewById(R.id.button_cancel);
+        button_add_to_cart = (Button) itemDetailPopupView.findViewById(R.id.button_add_to_cart);
+
+        dialogbuilder.setView(itemDetailPopupView);
+        dialog = dialogbuilder.create();
+        dialog.show();
+
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+          @Override
+            public void onClick(View v){
+              dialog.dismiss();
+          }
+        });
     }
 
     @Override
