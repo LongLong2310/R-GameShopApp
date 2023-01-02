650cd92f5983d1ca2f567e095713706267e39829
XcSteven <stevenbui1809@gmail.com>
2023-01-02 19:28:32 +0700

    Commit changes

--- app/src/main/java/com/example/r_gameshopapp/DatabaseManager.java
+++ app/src/main/java/com/example/r_gameshopapp/DatabaseManager.java
@@ -10,6 +10,7 @@ public class DatabaseManager {
     private DatabaseHelper dbHelper;
     private Context context;
     private SQLiteDatabase  database;
+    // Hi
 
     public DatabaseManager(Context c){
         context = c;
